fbd5a8a41126ebfbfbf2a54bfc501087f6cfa4f9
shigorin <136848770+shigorin@users.noreply.github.com>
2023-06-22 19:43:28 +0300

    Update connection.ts

--- flutter/web/js/src/connection.ts
+++ flutter/web/js/src/connection.ts
@@ -6,11 +6,8 @@ import * as sha256 from "fast-sha256";
 import * as globals from "./globals";
 import { decompress, mapKey, sleep } from "./common";
 
-const PORT = 21116;
+const PORT = 27116;
 const HOSTS = [
-  "rs-sg.rustdesk.com",
-  "rs-cn.rustdesk.com",
-  "rs-us.rustdesk.com",
 ];
 let HOST = localStorage.getItem("rendezvous-server") || HOSTS[0];
 const SCHEMA = "ws://";
